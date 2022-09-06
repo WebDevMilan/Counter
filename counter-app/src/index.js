a34595626079fb15d65110d7ff5b9183758f33da
Jesus Milan <milan.jesusdev@gmail.com>
2022-09-05 22:25:53 -0400

    learing basics

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Counter />
   </React.StrictMode>
 );
