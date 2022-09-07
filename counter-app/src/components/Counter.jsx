e76901f80ea101bbb407668137f7eff65841defc
Jesus Milan <milan.jesusdev@gmail.com>
2022-09-06 22:01:03 -0400

    raising and handling events

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -2,24 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = (product) => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.counter.value,
   };
 
   render() {
@@ -32,11 +30,7 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
